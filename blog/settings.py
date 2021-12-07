c0794fe012922e95a8058b51e8eac0ce2af09783
earthkid0 <ghs1541@naver.com>
2021-12-07 20:34:57 +0900

    enter

--- blog/settings.py
+++ blog/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog_open',
-    'single_pages',
+    
 ]
 
 MIDDLEWARE = [
