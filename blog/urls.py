46f4a839905c86d8e70d0ffe754c58608a3ae0da
earthkid0 <ghs1541@naver.com>
2021-12-09 22:39:32 +0900

    enter

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
+    path('blog2/',include('blog2.urls')),
     path('admin/', admin.site.urls),
 ]
