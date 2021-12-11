d7ecf0749fa8a544c0228e2c79c594e6742b4999
earthkid0 <ghs1541@naver.com>
2021-12-11 20:20:49 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import render
 
 # Create your models here.
 class Post(models.Model):
@@ -11,3 +12,6 @@ class Post(models.Model):
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
+    
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
