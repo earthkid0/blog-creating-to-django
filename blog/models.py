8fd44455f5675968ee62bef134875812da586a16
earthkid0 <ghs1541@naver.com>
2021-12-11 20:14:43 +0900

    enter

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     #author = 
 
