a476abe45a41bffcfa4f37f89d2cd613ea896a2d
earthkid0 <ghs1541@naver.com>
2021-12-12 21:04:30 +0900

    enter

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+#FBV 방식
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
 
-def single_post_page(request,pk):
-    post = Post.objects.get(pk=pk)
+# def single_post_page(request,pk):
+#     post = Post.objects.get(pk=pk)
 
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post' : post,
-        }
-    )
-    
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post' : post,
+#         }
+#     )
+
+# CBV 방식
+
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/index.html'
