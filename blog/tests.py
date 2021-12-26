63b54abf22028797b1c2582239573d209abc2245
earthkid0 <ghs1541@naver.com>
2021-12-26 20:02:49 +0900

    enter

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.test.client import Client
 
 # Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_list(self):
+        # 1.1 포스트 목록 페이지를 가져온다.
+        # 1.2 정상적으로 페이지가 로드된다.
+        
