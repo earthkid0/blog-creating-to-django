2710a1a0f85ef85841946ddbc1532417a901ca86
earthkid0 <ghs1541@naver.com>
2022-01-01 20:14:12 +0900

    포스트 목록, 상세 페이지에서 작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,13 +1,15 @@
-from django.http import response
 from django.test import TestCase
 from django.test.client import Client
 from bs4 import BeautifulSoup
 from .models import Post
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_earthkid0 = User.objects.create_user(username='earthkid0',password='somepassword')
+        self.user_deadbort = User.objects.create_user(username='deadbort',password='somepassword')
 
     def navbar_test(self,soup):
         navbar = soup.nav
@@ -26,7 +28,7 @@ class TestView(TestCase):
         about_me_btn = navbar.find('a',text='About Me')
         self.assertEqual(about_me_btn.attrs['href'],'/about_me/')
 
-        
+
     def test_post_list(self):
         # 1.1 포스트 목록 페이지를 가져온다.
         response = self.client.get('/blog/')
@@ -49,10 +51,12 @@ class TestView(TestCase):
         post_001 = Post.objects.create(
             title = '첫 번째 포스트입니다.',
             content = 'Hello World. We are the world.',
+            author = self.user_earthkid0,
         )
         post_002 = Post.objects.create(
             title = '두 번째 포스트입니다.',
             content = '1등이 전부는 아니잖아요?',
+            author = self.user_deadbort,
         )
         # 3.2 포스트 목록 페이지를 새로고침했을 때
         response = self.client.get('/blog/')
@@ -65,11 +69,15 @@ class TestView(TestCase):
         # 3.4 "아직 게시물이 없습니다" 라는 문구는 더 이상 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
 
+        self.assertIn(self.user_earthkid0.username.lower(), main_area.text)
+        self.assertIn(self.user_deadbort.username.lower(), main_area.text)
+
     def test_post_detail(self):
         # 1.1 포스트가 하나 있다.
         post_001 = Post.objects.create(
             title = "첫 번째 포스트입니다.",
             content = "Hellow World. We are the world.",
+            author = self.user_earthkid0,
         )
         # 1.2 그 포스트의 url은 '/blog/1/' 이다.
         self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
@@ -88,6 +96,6 @@ class TestView(TestCase):
         post_area = main_area.find('div', id='post-area')
         self.assertIn(post_001.title, post_area.text)
         # 2.5 첫 번째 포스트의 작성자(author)가 포스트 영영에 있다(아직 구현할 수 없음).
-        #작성 불가
+        self.assertIn(self.user_earthkid0.username.lower(),post_area.text)
         # 2.6 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.    
         self.assertIn(post_001.content, post_area.text)
