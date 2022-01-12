7780a53cda41a50c526eb7b79bf06447bd3a13af
earthkid0 <ghs1541@naver.com>
2022-01-12 18:29:10 +0900

    댓글 폼으로 포스트 상세 페이지 댓글 작성하기

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from os import name
+import django
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Category, Post, Tag
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
+from .forms import CommentForm
+from django.shortcuts import get_object_or_404
 # Create your views here.
 #FBV 방식
 # def index(request):
@@ -162,6 +165,22 @@ class PostDetail(DetailView):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['comment_form'] = CommentForm
         return context
 
+def new_comment(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
 
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+        else:
+            return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
