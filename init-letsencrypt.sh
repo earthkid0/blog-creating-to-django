59be3862f843d9f259ac5f881b171e7493274b09
earthkid0 <ghs1541@naver.com>
2022-01-18 21:24:09 +0900

    letsencrypt 연결하기

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(xn--299as2gj7c1yy6wb.com www.xn--299as2gj7c1yy6wb.com)
+domains=(고옥들농장.com www.고옥들농장.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="ghs1541@naver.com"
