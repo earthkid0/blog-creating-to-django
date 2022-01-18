6d2bf503c61f60f0de64db00b614e48a0220d6a4
earthkid0 <ghs1541@naver.com>
2022-01-18 21:41:37 +0900

    avatar 설정

--- blog/models.py
+++ blog/models.py
@@ -76,7 +76,7 @@ class Comment(models.Model):
         if self.author.socialaccount_set.exists():
             return self.author.socialaccount_set.first().get_avatar_url()
         else:
-            return 'http://placehold.it/50x50'
+            return f'https://doitdjango.com/avatar/id/533/f666100f8b8b2cb4/svg/{self.author.email}'
 
     def __str__(self):
         return f'{self.author}::{self.content}'
