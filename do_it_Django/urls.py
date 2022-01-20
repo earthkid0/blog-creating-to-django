2fc50da27d5824eec3cda5059b3174d3324da9cd
earthkid0 <ghs1541@naver.com>
2022-01-21 01:53:03 +0900

    naver

--- do_it_Django/urls.py
+++ do_it_Django/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.views.generic import TemplateView
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -25,6 +26,7 @@ urlpatterns = [
     path('markdownx/', include('markdownx.urls')),
     path('accounts/', include('allauth.urls')),
     path('',include('single_pages.urls')),
+    path('robots.txt',  TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
