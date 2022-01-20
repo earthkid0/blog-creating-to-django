3d4b0cd98990736dda24f64d00aa3a875423e47d
earthkid0 <ghs1541@naver.com>
2022-01-21 02:29:53 +0900

    naver

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 from . import views
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('about_me/', views.about_me),
     path('',views.landing),
-    path('robots.txt/', TemplateView.as_view(template_name="robots.txt", 
-         content_type='text/plain'))
+    
 ]
