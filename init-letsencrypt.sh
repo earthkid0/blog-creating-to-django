9466add51b334d3298eab58ee8fb8ec17575338d
earthkid0 <ghs1541@naver.com>
2022-01-21 00:52:33 +0900

    naver

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(xn--299as2gj7c1yy6wb.com www.xn--299as2gj7c1yy6wb.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="ghs1541@naver.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
